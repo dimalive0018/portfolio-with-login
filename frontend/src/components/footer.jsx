5361dc51e34b93e455a3425024cb7c187b6766d4
liviodimola <liviodimola@gmail.com>
2023-09-15 01:24:01 +0200

    Update footer.jsx

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className='bg-[#6B5B95] w-full h-6 flex items-center justify-around text-center text-lg bottom-0 fixed z-0'>
             <h3 className='text-white'>© Livio Dimola</h3>
-            <a href='https://codesandbox.io/p/github/liviodimola/portfolio-with-login/main?workspaceId=bb702a07-4889-46e1-b6f7-b4bb8104efbe' target='_blank' className='text-white'>Source code portfolio</a>
+            <a href='https://codesandbox.io/p/github/liviodimola/portfolio-with-login/main' target='_blank' className='text-white'>Source code portfolio</a>
         </footer>
     )
 }
